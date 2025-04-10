d6ab9e847007ff278bef8d4f9dd661d90b13f69a
tylerjohnsononline <tylerjohnsononline@gmail.com>
2025-04-10 13:17:52 -0700

    found problem when importing numpy

---
+++ jpn_vocab.py
@@ -0,0 +1,33 @@
+
+import random as r
+import numpy as np
+import pandas as pd
+import io
+
+
+class User(): 
+  def __init__(self):
+    pass
+    # self.data_path = 
+    # self.words_df= pd.DataFrame(self.data_path) 
+    # self.
+    # self.active_word
+  def random_word(self):
+    # incomplete
+    pass
+
+
+
+# gui, view
+# character = data.loc[random_word_index, "japanese" ]
+# romanji = data.loc[random_word_index, "romanji" ]
+# english = data.loc[random_word_index, "english" ]
+
+# print("what does", character, "mean?")
+# print(character, "is sounded out", romanji)
+# print(character, "means", english)
+class Display():
+  def __init__(self):
+    pass
+  
+
